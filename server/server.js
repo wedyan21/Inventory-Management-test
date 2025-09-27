3fd3a259118d0783ea0e0fb590068132b4f3868b
wedyan21 <59253642+wedyan21@users.noreply.github.com>
2025-09-27 15:41:50 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://explosion-artificial-knife-every.trycloudflare.com/'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
