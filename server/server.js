7e27c0760780d8f85c43bcdbff3fbfd2c8090937
wedyan21 <59253642+wedyan21@users.noreply.github.com>
2025-09-27 15:30:00 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/api/health', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
